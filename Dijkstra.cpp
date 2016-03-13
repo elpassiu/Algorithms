197db1e5496e20d5425f7cd3112075a8c7317257
Eric Valls <eric.vallsg@gmail.com>
2016-03-13 10:27:52 +0100

    Update Dijkstra.cpp
    Implementation of Dijkstra's algorithm to find the shortest path from vertex 0 to all the other vertices in a weighted directed graph (without negative weights). It outputs "not connected" if there is no path between vertex 0 and the actual vertex. 
    Note that I'm changing the weights' sign when storing values in the priority queue so that it orders them non-decreasingly in absolute value.

--- Dijkstra.cpp
+++ Dijkstra.cpp
@@ -8,28 +8,28 @@ typedef pair <int, int> PII;
 
 int main () {
     int n, m, c, t, k;
-    cin >> n >> m;
-    priority_queue <PII> cua;
-    vector < int > visited (n, -1);
-    vector < vector < pair < int, int > > > v(n);
-    for (int i = 0; i < m; i++) {
+    cin >> n >> m;                  // n = number of vertices; m = number of edges;
+    vector < vector < PII > > v(n); // adjacency list of the graph. In this pair: "first" = vertex number; "second" = weight of the edge
+    for (int i = 0; i < m; i++) { 
         cin >> c >> t >> k;
-        v[c].push_back(make_pair(k ,t));
+        v[c].push_back(make_pair(t ,k));
     }
-    cua.push(make_pair(0,0));
+    vector < int > visited (n, -1); // vector where the weight of the path is stored. It stores -1 if it hasn't been visited yet
+    priority_queue <PII> cua;       //priority queue for Dijsktra's implementation. In this pair: "first" = vertex number; "second" = weight
+    cua.push(make_pair(0,0));       //inicialization of the priority queue with vertex 0 and weight 0
     while (not cua.empty()) {
         t = cua.top().first;
         k = cua.top().second;
         cua.pop();
-        if (visited[k] == -1) {
-            visited[k] = -t;
-            for (int i = 0; i < v[k].size(); i++) {
-                if (visited[v[k][i].second] == -1) cua.push(make_pair(t - v[k][i].first, v[k][i].second));
-            }
+        if (visited[k] == -1) {     // if the vertex has not yet been visited...
+            visited[k] = -t;        // we store the weight of the path, which we know to be the minimum
+            for (int i = 0; i < v[k].size(); i++) { // Then add to the priority queue all the vertices adjacent to the actual vertex
+                if (visited[v[k][i].first] == -1) cua.push(make_pair(t - v[k][i].second, v[k][i].first)); // that have not been visited yet
+            }                                   
         }
     }
     for (int i = 1; i < n; i++) {
-        if (visited[i] == -1) cout << "not connected" << endl;
-        else cout << visited[i] << endl;
+        if (visited[i] == -1) cout << i << " : not connected" << endl;
+        else cout << i << " : " << visited[i] << endl;
     }
 }
